6cdfa11d722c7b6c85807f1deb9b2e53f55c8b9e
tlsdla1235 <55173949+tlsdla1235@users.noreply.github.com>
2025-05-17 22:36:52 +0900

    feat : Add MovieActor Entity

---
+++ src/main/java/com/dbs/movieservice/domain/movie/MovieActor.java
@@ -0,0 +1,57 @@
+package com.dbs.movieservice.domain.movie;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+@Table(name="Movie_Actor")
+public class MovieActor {
+    @EmbeddedId
+    private MovieActorId id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("movieId")
+    @JoinColumn(name = "movie_id")
+    private Movie movie;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @MapsId("actorId")
+    @JoinColumn(name = "actor_id")
+    private Actor actor;
+
+    @Getter
+    @Setter
+    @Embeddable
+    public static class MovieActorId implements Serializable {
+
+        private Long movieId;
+        private Long actorId;
+
+        public MovieActorId() {}
+
+        public MovieActorId(Long movieId, Long actorId) {
+            this.movieId = movieId;
+            this.actorId = actorId;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof MovieActorId)) return false;
+            MovieActorId that = (MovieActorId) o;
+            return Objects.equals(movieId, that.movieId) &&
+                    Objects.equals(actorId, that.actorId);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(movieId, actorId);
+        }
+    }
+}
