2da45c544f4404cbcf43e63a0191b05ebc3979fa
tlsdla1235 <55173949+tlsdla1235@users.noreply.github.com>
2025-05-17 22:30:45 +0900

    feat: Add Customer Entity

---
+++ src/main/java/com/dbs/movieservice/domain/member/Customer.java
@@ -0,0 +1,43 @@
+package com.dbs.movieservice.domain.member;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+public class Customer {
+    @Id
+    @SequenceGenerator(name="customer_seq",sequenceName = "customer_seq",allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_seq")
+    @Column(name="customer_id")
+    private int customerId;
+
+    @Column(name = "customer_pw", length = 100, nullable = false)
+    private String customerPw;
+
+    @Column(name = "customer_name", length = 100, nullable = false)
+    private String customerName;
+
+    @Column(name = "birth_date")
+    private LocalDate birthDate;
+
+    @Column(name = "phone", length = 20)
+    private String phone;
+
+    @Column(name = "authority", length = 1)
+    private String authority;
+
+    @Column(name = "join_date")
+    private LocalDate joinDate;
+
+    @Column(name = "points")
+    private Integer points;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "level_id", nullable = false)
+    private Level level;
+}
