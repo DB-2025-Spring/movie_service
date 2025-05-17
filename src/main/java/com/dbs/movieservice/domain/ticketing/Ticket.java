e631d28aecdd15ca4bc2d198a0969ed7090d9df5
tlsdla1235 <55173949+tlsdla1235@users.noreply.github.com>
2025-05-18 06:12:18 +0900

    feat: Add Ticket Entity

---
+++ src/main/java/com/dbs/movieservice/domain/ticketing/Ticket.java
@@ -0,0 +1,46 @@
+package com.dbs.movieservice.domain.ticketing;
+
+
+import com.dbs.movieservice.domain.member.Customer;
+import com.dbs.movieservice.domain.theater.Schedule;
+import com.dbs.movieservice.domain.theater.Seat;
+import com.dbs.movieservice.domain.theater.Theater;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "ticket")
+public class Ticket {
+    @Id
+    @SequenceGenerator(name = "ticket_seq", sequenceName = "ticket_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ticket_seq")
+    @Column(name = "ticket_id")
+    private Long ticketId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "schedule_id", nullable = false)
+    private Schedule schedule;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumns({
+            @JoinColumn(name = "seat_id", referencedColumnName = "seat_id", nullable = false),
+            @JoinColumn(name = "theater_id", referencedColumnName = "theater_id", nullable = false)
+    })
+    private Seat seat;
+
+    @Column(name = "is_issued", length = 1, nullable = false)
+    private String isIssued;
+
+    @Column(name = "booking_datetime", nullable = false)
+    private LocalDateTime bookingDatetime;
+
+}
