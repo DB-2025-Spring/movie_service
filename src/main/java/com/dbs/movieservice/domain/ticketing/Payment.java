46ba97cd385b53c9f073d4f7211e8d71da8f9ba6
tlsdla1235 <55173949+tlsdla1235@users.noreply.github.com>
2025-05-18 06:14:44 +0900

    feat: Add Payment Entity

---
+++ src/main/java/com/dbs/movieservice/domain/ticketing/Payment.java
@@ -0,0 +1,49 @@
+package com.dbs.movieservice.domain.ticketing;
+
+import com.dbs.movieservice.domain.member.Customer;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "payment")
+public class Payment {
+    @Id
+    @SequenceGenerator(name = "payment_seq", sequenceName = "payment_seq", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "payment_seq")
+    @Column(name = "payment_id")
+    private Long paymentId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ticket_id", nullable = false)
+    private Ticket ticket;
+
+    @Column(name = "payment_amount", nullable = false)
+    private Integer paymentAmount;
+
+    @Column(name = "payment_method", length = 20, nullable = false)
+    private String paymentMethod;
+
+    @Column(name = "approval_number")
+    private Integer approvalNumber;
+
+    @Column(name = "payment_status", length = 20)
+    private String paymentStatus;
+
+    @Column(name = "payment_date")
+    private LocalDate paymentDate;
+
+    @Column(name = "discount_amount")
+    private Integer discountAmount;
+
+    @Column(name = "used_points", length = 18)
+    private String usedPoints;
+}
