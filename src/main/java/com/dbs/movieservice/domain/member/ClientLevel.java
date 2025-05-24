99e2d38c4a5422794047434984bbc7c2346ebc4f
ye0314 <117622694+youngeun0314@users.noreply.github.com>
2025-05-25 00:26:41 +0900

    chore : ClientLevel, Customer
    - customerId의 타입을 int -> Long
    - ClientLevel 엔티티에 name 속성 추가
    - level_id를 자동으로 생성하는 부분 삭제

--- src/main/java/com/dbs/movieservice/domain/member/ClientLevel.java
+++ src/main/java/com/dbs/movieservice/domain/member/ClientLevel.java
@@ -2,18 +2,28 @@ package com.dbs.movieservice.domain.member;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
 @Table(name="client_level")
 public class ClientLevel {
     @Id
-    @SequenceGenerator(name = "level_seq", sequenceName = "level_seq", allocationSize = 1)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "level_seq")
     @Column(name="level_id")
     private Integer levelId;
+
+    @Column(name="level_name", length = 50, nullable = false)
+    private String levelName;
+
     @Column(name="reward_rate")
     private Double rewardRate;
+
+    public ClientLevel(Integer levelId, String levelName, Double rewardRate) {
+        this.levelId = levelId;
+        this.levelName = levelName;
+        this.rewardRate = rewardRate;
+    }
 }
